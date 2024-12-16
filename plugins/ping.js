b13422fd1bbae1b0c9f709f00b3b60550e1b2c86
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-16 19:36:05 +0200

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -47,7 +47,7 @@ l(e)
 
 cmd({
     pattern: "ping",
-    react: "♻️",
+    react: "🚀",
     alias: ["speed"],
     desc: "Check bot\'s ping",
     category: "main",
@@ -57,10 +57,10 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '*_🪄Pinging..._*' })
+        const message = await conn.sendMessage(from, { text: '*_🔃Pinging..._*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*♻️ Speed... : ${ping}ms*`}, { quoted: message })
+        await conn.sendMessage(from, { text: `*❄️SUBZERO'S Speed... : ${ping}ms*`}, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
