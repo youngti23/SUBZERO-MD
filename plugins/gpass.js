ae93ac5707d7938aafec6a35b4a8971b010753a9
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-18 12:44:58 +0200

    Update gpass.js

--- plugins/gpass.js
+++ plugins/gpass.js
@@ -49,7 +49,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         };
 
         const password = generatePassword(length);
-        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n *BY KERM_MD-V4*`;
+        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n *BY SUBZERO MD*`;
 
         // Send initial notification message
         await conn.sendMessage(from, { text: message }, { quoted: mek });
