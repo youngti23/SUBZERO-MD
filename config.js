c11083eed9bf03ed1814b614da374cee755eac3b
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-24 12:22:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -25,8 +25,10 @@ function convertToBool(text, fault = 'true') {
 
 //--------------------------Mr Frank---------------------------------------
 module.exports = {
+
 SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SUBZEFOSESSION HERE",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
+OWNER_NUMBER: process.env.OWNER_NUMBER || "18062212660",
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
